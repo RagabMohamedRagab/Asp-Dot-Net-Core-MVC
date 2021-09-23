e609842b187c2b201605bf030f28dd00cadf1a9e
Ragab Muhamed Ragab <mohamaragab0@gmail.com>
2021-09-23 18:14:31 +0200

    CURUD OPeration In MVC core

---
+++ DotNetCoreMvc/Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using DotNetCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Create()
+        
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            Repository.Create(product);
+            return View("Thanks", product);
+        }
+        public IActionResult Display()
+        {
+            return View(Repository.AllProducts);
+        }
+        public IActionResult Edit(int id)
+        {
+            var ProductId = Repository.AllProducts.Where(pId => pId.Id == id).FirstOrDefault();
+            if (ProductId != null)
+            {
+                return View(ProductId);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product,int id)
+        {
+            Repository.AllProducts.Where(product => product.Id == id).FirstOrDefault().Id = product.Id;
+            Repository.AllProducts.Where(product => product.Id == id).FirstOrDefault().Name = product.Name;
+            Repository.AllProducts.Where(product => product.Id == id).FirstOrDefault().Age = product.Age;
+            return RedirectToAction("Display");
+        }
+        public IActionResult Delet(int empID)
+        {
+            Repository.Delete(empID);
+            return RedirectToAction("Display");
+        }
+       
+    
+        public IActionResult Index(int id)
+        {
+            var All = Repository.AllProducts.Where(product => product.Id == id).FirstOrDefault();
+            return View("Index",All);
+        }
+       
+        
+
+    }
+}
---
+++ DotNetCoreMvc/Models/Repository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMvc.Models
+{
+    public static class Repository
+    {
+        static List<Product> Products = new List<Product>();
+        public static ICollection<Product> AllProducts { get => Products; }
+        public static void Create(Product product )
+        {
+            Products.Add(product);
+        }
+        public static void Delete(int id)
+        {
+            Products.Remove(Repository.AllProducts.Where(p=>p.Id==id).FirstOrDefault());
+        }
+        public static List<Product> Search(string Name)
+        {
+            return Repository.AllProducts.Where(product => product.Name == Name).ToList();
+        }
+        
+    }
+}
